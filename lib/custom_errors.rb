1ac80e355d4d04e59cf7923550860f3c4f4de5bb
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-19 08:16:31 +0300

    commit tree 6308aec136a0cf2e5397f05117f760b06eb777c2

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,8 +6,22 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
